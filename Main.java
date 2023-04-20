c13df7fb6d6513efea54bc01998a3fcbe4af6608
yy05853 <20264868-yy05853@users.noreply.replit.com>
2023-04-20 14:05:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("入力してください> ");    
+    String memo = scanner.nextLine();
+
+    System.out.println(memo);        
+  }
+}
