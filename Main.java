35fe5b29224ea4803aac7f6b06715a3a5c18e0ae
yy05853 <44853475+yy05853@users.noreply.github.com>
2023-04-22 06:26:44 +0000

    句読点を追加した

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("入力してください> ");    
+    System.out.print("入力してください。> ");    
     String memo = scanner.nextLine();
 
     System.out.println(memo);        
